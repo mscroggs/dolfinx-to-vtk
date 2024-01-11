e791b651277136090281e2e9a4e1ea61a00de2c8
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-01-11 16:19:06 +0000

    correct

--- perms.py
+++ perms.py
@@ -1,51 +1,25 @@
 def hex(degree):
-    dof_n = 0
-
-    dofs_on_entities = [[], [], [], []]
-    for vertex in range(8):
-        dofs_on_entities[0].append([dof_n])
-        dof_n += 1
-    for edge in range(12):
-        dofs = []
-        for i in range(degree - 1):
-            dofs.append(dof_n)
-            dof_n += 1
-        dofs_on_entities[1].append(dofs)
-    for face in range(6):
-        dofs = []
-        for i in range((degree - 1) ** 2):
-            dofs.append(dof_n)
-            dof_n += 1
-        dofs_on_entities[2].append(dofs)
-    dofs = []
-    for i in range((degree - 1) ** 3):
-        dofs.append(dof_n)
-        dof_n += 1
-    dofs_on_entities[3].append(dofs)
-
     p = []
     for v in [0, 1, 3, 2, 4, 5, 7, 6]:
-        p += dofs_on_entities[0][v]
+        p += [v]
+    base = 8
     if degree > 1:
-        #for edge, reverse in [
-        #    (0, False), (3, True), (11, False), (1, False), (8, False), (2, False),
-        #    (10, False), (9, False), (4, False), (7, True), (5, False), (6, False),
-        #]:
-        for edge, reverse in [
-            (0, False), (3, False), (5, False), (1, True), (8, False), (10, False),
-            (11, False), (9, True), (2, False), (4, False), (7, False), (6, False),
-        ]:
-            if reverse:
-                p += dofs_on_entities[1][edge][::-1]
-            else:
-                p += dofs_on_entities[1][edge]
+        n = degree - 1
+        for edge in [0, 3, 5, 1, 8, 10, 11, 9, 2, 4, 7, 6]:
+            for i in range(n):
+                p.append(base + n * edge + i)
 
+        base += 12 * n
+
+        n = (degree - 1) * (degree - 1)
         for face in [2, 3, 1, 4, 0, 5]:
-            p += dofs_on_entities[2][face]
+            for i in range(n):
+                p.append(base + n * face + i)
+        base += 6 * n
 
-        p += dofs_on_entities[3][0]
-
-    print(p)
+        n = (degree - 1) * (degree - 1) * (degree - 1)
+        for i in range(n):
+            p.append(base + i)
 
     return p
 
