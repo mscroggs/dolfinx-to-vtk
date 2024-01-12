278e9ff126a15841b1ca249030dfd695d41a7079
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-01-12 18:56:51 +0000

    working on tets

--- perms.py
+++ perms.py
@@ -1,3 +1,10 @@
+import math
+
+
+def int_sqrt(n):
+    return int(math.floor(math.sqrt(n)))
+
+
 def hex(degree):
     p = []
     for v in [0, 1, 3, 2, 4, 5, 7, 6]:
@@ -24,33 +31,195 @@ def hex(degree):
     return p
 
 
+def tri(degree):
+    num_nodes = (degree + 1) * (degree + 2) // 2
+    out = [0, 1, 2]
+    if degree == 1:
+        return out
+    base = 3
+    n = degree - 1
+    for edge in [2, 0, 1]:
+        if edge == 1:
+            for i in range(n):
+                out.append(base + edge * n + n - 1 - i)
+        else:
+            for i in range(n):
+                out.append(base + edge * n + i)
+    base += 3 * n
+
+    if base < num_nodes:
+        remainders = list(range(num_nodes - base))
+
+        out += [base + i for i in tri_remainders(remainders)]
+
+    return out
+
+
+def tri_remainders(remainders):
+    out = []
+    while len(remainders) > 0:
+        if len(remainders) == 1:
+            out.append(remainders[0])
+            remainders = []
+            break
+
+        deg = (int_sqrt(1 + 8 * len(remainders)) - 1) // 2 - 1
+
+        out.append(remainders[0])
+        remainders = remainders[1:]
+        out.append(remainders[deg - 1])
+        remainders = remainders[:deg - 1] + remainders[deg:]
+        out.append(remainders[-1])
+        remainders = remainders[:-1]
+
+        for _ in range(deg - 1):
+            out.append(remainders[0])
+            remainders = remainders[1:]
+
+        k = deg * (deg - 1) // 2
+        for i in range(deg - 1):
+            out.append(remainders[-k])
+            if k == 1:
+                remainders = remainders[:-k]
+            else:
+                remainders = remainders[:-k] + remainders[-k+1:]
+            k -= deg - i - 1
+
+        k = 1
+        for i in range(deg - 1):
+            out.append(remainders[-k])
+            if k == 1:
+                remainders = remainders[:-k]
+            else:
+                remainders = remainders[:-k] + remainders[-k+1:]
+            k += i + 1
+
+    return out
+
+
 def tet(degree):
     p = []
     for vertex in [0, 1, 2, 3]:
         p.append(vertex)
 
+    if degree == 1:
+        return p
+
     base = 4
-    if degree > 1:
-        n = degree - 1
-        for edge in [5, 2, 4, 3, 1, 0]:
-            if edge == 4:
-                for i in range(n):
-                    p.append(base + n * edge + n - 1 - i)
-            else:
-                for i in range(n):
-                    p.append(base + n * edge + i)
-
-        base += 6 * n
-    print(p)
-    if degree > 2:
-        n = (degree - 1) * (degree - 2) // 2
-        for face in [2, 0, 1, 3]:
+    n = degree - 1
+    for edge in [5, 2, 4, 3, 1, 0]:
+        if edge == 4:
             for i in range(n):
-                p.append(base + n * face + i)
-        base += 4 * n
-    if degree > 3:
-        n = (degree - 1) * (degree - 2) * (degree - 3) // 6
-        for i in range(n):
-            p.append(base + i)
+                p.append(base + n * edge + n - 1 - i)
+        else:
+            for i in range(n):
+                p.append(base + n * edge + i)
 
+    base += 6 * n
+
+    if degree <= 2:
+        return p
+
+    n = (degree - 1) * (degree - 2) // 2
+    face_order = tri(n)
+    for face in [2, 0, 1, 3]:
+        face_dofs = []
+        if face == 2:
+            for i in range(n):
+                face_dofs.append(base + n * face + i)
+        elif face == 0:
+            # rot1
+            face_dofs = []
+            for i in range(degree - 3, -1, -1):
+                d = i
+                for j in range(i + 1):
+                    face_dofs.append(base + n * face + d)
+                    d += degree - 3 - j
+        else:
+            # ref1
+            for i in range(degree - 2):
+                d = i
+                for j in range(degree - 2 - i):
+                    face_dofs.append(base + n * face + d)
+                    d += degree - 2 - j
+        p += tri_remainders(face_dofs)
+    base += 4 * n
+
+    if degree <= 3:
+        return p
+
+    n = (degree - 1) * (degree - 2) * (degree - 3) // 6
+    remainders = [base + i for i in range(n)]
+
+    p += tet_remainders(remainders)
     return p
+
+
+def tet_remainders(remainders):
+    out = []
+    while len(remainders) > 0:
+        if len(remainders) == 1:
+            out.append(remainders[0])
+            remainders = []
+            break
+
+        deg = 0
+        while deg * (deg + 1) * (deg + 2) < 6 * len(remainders):
+            deg += 1
+
+        for dof in [0, deg - 2, deg * (deg + 1) // 2 - 3, -1]:
+            out.append(remainders[dof])
+            if dof + 1 in [0, len(remainders)]:
+                remainders = remainders[:dof]
+            else:
+                remainders = remainders[:dof] + remainders[dof + 1:]
+
+        if deg > 2:
+            for i in range(deg - 2):
+                out.append(remainders[0])
+                remainders = remainders[1:]
+            d = deg - 2
+            for i in range(deg - 2):
+                out.append(remainders[d])
+                remainders = remainders[:d] + remainders[d + 1:]
+                d += deg - 3 - i
+            d = deg * (deg - 3) // 2
+            for i in range(deg - 2):
+                out.append(remainders[d])
+                remainders = remainders[:d] + remainders[d + 1:]
+                d -= deg - 3 - i
+            d = (deg - 3) * (deg - 2) // 2
+            for i in range(deg - 2):
+                out.append(remainders[d])
+                remainders = remainders[:d] + remainders[d + 1:]
+                d += (deg - i) * (deg - i - 1) // 2  - 1
+            d = (deg - 3) * (deg - 2) // 2 + deg - 3
+            for i in range(deg - 2):
+                out.append(remainders[d])
+                remainders = remainders[:d] + remainders[d + 1:]
+                d += (deg - 2 - i) * (deg - 1 - i) // 2 + deg - 4 - i
+            d = (deg - 3) * (deg - 2) // 2 + deg - 3 + (deg - 2) * (deg - 1) // 2 - 1
+            for i in range(deg - 2):
+                out.append(remainders[d])
+                remainders = remainders[:d] + remainders[d + 1:]
+                d += (deg - 3 - i) * (deg - 2 - i) // 2 + deg - i - 5
+
+        if deg > 3:
+            dofs = []
+            d = (deg - 3) * (deg - 2) // 2
+            for i in range(deg - 3):
+                for j in range(deg - 3 - i):
+                    print(d)
+                    dofs.append(remainders[d])
+                    d += 1
+                d += (deg - 2 - i) * (deg - 1 - i) // 2
+
+            print(dofs)
+            remainders += tri_remainders(dofs)
+
+            # TODO
+
+            print(deg, "r", len(remainders))
+        print()
+
+    return out
--- test/test_perms.py
+++ test/test_perms.py
@@ -1,12 +1,40 @@
+import pytest
 import perms
 
 
 def check_equal(a, b):
+    print(a)
+    print(b)
     assert len(a) == len(b)
     for i, j in zip(a, b):
-        print(i, j)
         assert i == j
 
+
+@pytest.mark.parametrize("degree", range(1, 9))
+def test_tri(degree):
+    target = [0, 1, 2]
+    j = 3
+    target += [2 * degree + k for k in range(1, degree)]
+    target += [2 + k for k in range(1, degree)]
+    target += [2 * degree + 1 - k for k in range(1, degree)]
+    if degree == 3:
+        target += [len(target) + i for i in [0]]
+    elif degree == 4:
+        target += [len(target) + i for i in [0, 1, 2]]
+    elif degree == 5:
+        target += [len(target) + i for i in [0, 2, 5, 1, 4, 3]]
+    elif degree == 6:
+        target += [len(target) + i for i in [0, 3, 9, 1, 2, 6, 8, 7, 4, 5]]
+    elif degree == 7:
+        target += [len(target) + i for i in [0, 4, 14, 1, 2, 3, 8, 11, 13, 12, 9, 5, 6, 7, 10]]
+    elif degree == 8:
+        target += [len(target) + i for i in [0, 5, 20, 1, 2, 3, 4, 10, 14, 17, 19,
+                                             18, 15, 11, 6, 7, 9, 16, 8, 13, 12]]
+    elif degree == 9:
+        target += [len(target) + i for i in [0, 6, 27, 1, 2, 3, 4, 5, 12, 17, 21, 24, 26, 25,
+                                             22, 18, 13, 7, 8, 11, 23, 9, 10, 16, 20, 19, 14, 15]]
+
+    check_equal(perms.tri(degree), target)
 
 def test_hex_degree_1():
     check_equal(perms.hex(1), [0, 1, 3, 2, 4, 5, 7, 6])
@@ -44,12 +72,13 @@ def test_tet_degree_4():
     check_equal(perms.tet(4), [
         0, 1, 2, 3,
         19, 20, 21, 10, 11, 12, 18, 17, 16, 13, 14, 15, 7, 8, 9, 4, 5, 6,
-        28, 29, 30, 22, 23, 24, 25, 26, 27, 31, 32, 33,
+        28, 29, 30, 23, 24, 22, 25, 27, 26, 31, 33, 32,
         34
     ])
 
-def test_tet_degree_5():
-    assert len(perms.tet(5)) == 7*8
 
-def test_tet_degree_6():
-    assert len(perms.tet(6)) == 7*8*9//6
+@pytest.mark.parametrize("degree", range(5, 10))
+def test_tet(degree):
+    p = perms.tet(degree)
+    assert len(p) == (degree + 1) * (degree + 2) * (degree + 3) // 6
+    assert len(set(p)) == len(p)
